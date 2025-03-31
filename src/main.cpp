d3765314225cd939d9ce632088581e19da275024
DuckyDuck69 <phanthaihoangduc99@gmail.com>
2025-03-30 23:04:57 -0400

    hihi

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 #include <stdio.h>
 
+std::string typeCheck(std::string s){
+  std::string result;
+  if(s == "echo" || s == "exit" || s == "type"){
+    result = s + " is a shell builtin";
+  }
+  else{
+    result = s + ": not found";
+  }
+  return result;
+}
+
 int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
@@ -15,14 +26,21 @@ int main() {
 
 
     std::string echoCommand = "echo";
-    size_t findEcho = input.find(echoCommand);
+    std::string exitCommand = "exit 0";
+    std::string typeCommand = "type";
+    size_t findEcho = input.find(echoCommand);   //return a number, which is the index of the first character it found
+    size_t findType = input.find(typeCommand);
 
-    if(input == "exit 0"){
+    if(input == exitCommand){ //exit if the user type "exit 0"
       loop = false;
     }
-    else if( findEcho != std::string::npos){
+    else if( findEcho == 0){   //declare echo command, which is to print out a string
       input.erase(0, echoCommand.length() + 1);
       std::cout<<input<<std::endl;
+    }
+    else if(findType == 0){
+      input.erase(0, typeCommand.length() + 1);
+      std::cout<<typeCheck(input)<<std::endl;
     }
     else{
       std::cout<<input<<": command not found"<<std::endl;
