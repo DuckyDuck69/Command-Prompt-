c77af7dc3fc62c79c3df2d105d49faf4a8f441e1
DuckyDuck69 <phanthaihoangduc99@gmail.com>
2025-03-30 20:43:25 -0400

    hi

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stdio.h>
 
 int main() {
   // Flush after every std::cout / std:cerr
@@ -12,8 +13,16 @@ int main() {
     std::string input;
     std::getline(std::cin, input);
 
+
+    std::string echoCommand = "echo";
+    size_t findEcho = input.find(echoCommand);
+
     if(input == "exit 0"){
       loop = false;
+    }
+    else if( findEcho != std::string::npos){
+      input.erase(0, echoCommand.length());
+      std::cout<<input<<std::endl;
     }
     else{
       std::cout<<input<<": command not found"<<std::endl;
